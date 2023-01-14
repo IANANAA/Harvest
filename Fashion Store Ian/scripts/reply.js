992dcc09a1b2b5ae03dc162007df1fea7695ff55
Reyhan Pramudita <reyhanpramudita@gmail.com>
2023-01-14 20:19:52 +0700

    add portofolio ian

---
+++ Fashion Store Ian/scripts/reply.js
@@ -0,0 +1,8 @@
+function replyF1() {
+  var x = document.getElementById("reply-1");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
