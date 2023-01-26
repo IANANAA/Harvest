4f9a664a8e1467e22e745e28cd2a2dfd35c17dac
Reza <rezafadhilah29@gmail.com>
2023-01-26 11:00:29 +0700

    fix

--- Fashion Store Ian/scripts/reply.js
+++
@@ -1,8 +0,0 @@
-function replyF1() {
-  var x = document.getElementById("reply-1");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
